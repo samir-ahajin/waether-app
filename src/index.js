1088e6630c24f61b3e37643d6b391ac1c466b62c
Samir Ahajin <samirahajin@gmail.com>
2022-12-13 17:06:05 +0800

    updated a code

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import mainWeatherInfo from "./getInfo";
 import details from "./showInfo";
 let sbutton = document.getElementById("search-btn");
 
+(async () => {
+  let sval = "manila";
+
+  let searchData = await mainWeatherInfo.data(sval);
+  details.show(searchData);
+})();
 sbutton.addEventListener("click", async () => {
   let sval = document.getElementById("search").value;
 
   let searchData = await mainWeatherInfo.data(sval);
   details.show(searchData);
 });
-
-window.onload = async () => {
-  let sval = "manila";
-
-  let searchData = await mainWeatherInfo.data(sval);
-  details.show(searchData);
-};
